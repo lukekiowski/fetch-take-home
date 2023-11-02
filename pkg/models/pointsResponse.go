702b0b151b630ee2d93949f3c3d5fb3290f1113d
Luke <lkiowski@gmail.com>
2023-11-01 20:31:12 -0500

    create model structs

---
+++ pkg/models/pointsResponse.go
@@ -0,0 +1,5 @@
+package models
+
+type PointsResponse struct {
+    Points int `json:"points"`
+}
---
+++ pkg/models/receiptCreatedResponse.go
@@ -0,0 +1,5 @@
+package models
+
+type ReceiptCreatedResponse struct {
+    Id string `json:"id"`
+}
